d032358e89c373cc5db553507467939414715221
Tawnie Bavaro <tawniebavaro@gmail.com>
2021-04-05 17:58:48 +0100

    build

---
+++ build/precache-manifest.b1196425b78fadb5285721d237a5de95.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52768120e100b03feafeeaeb93ea1295",
+    "url": "/sbhairdesign-marketingsite/index.html"
+  },
+  {
+    "revision": "b308bb8e8440bbc83f3e",
+    "url": "/sbhairdesign-marketingsite/static/css/main.4b66ff21.chunk.css"
+  },
+  {
+    "revision": "b9067725ccd5a52f604d",
+    "url": "/sbhairdesign-marketingsite/static/js/2.e976e00f.chunk.js"
+  },
+  {
+    "revision": "a3c66bc87fa121efed0250fdac2dfa2b",
+    "url": "/sbhairdesign-marketingsite/static/js/2.e976e00f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b308bb8e8440bbc83f3e",
+    "url": "/sbhairdesign-marketingsite/static/js/main.cc5afde1.chunk.js"
+  },
+  {
+    "revision": "023a4fd1384b3c8e6e8d",
+    "url": "/sbhairdesign-marketingsite/static/js/runtime-main.f0fb5d9b.js"
+  },
+  {
+    "revision": "2811657203086ef7afffadbddd414382",
+    "url": "/sbhairdesign-marketingsite/static/media/LOGO.28116572.png"
+  },
+  {
+    "revision": "158a377f97cb8d44b2df2abc9b2ce147",
+    "url": "/sbhairdesign-marketingsite/static/media/LeafBackground.158a377f.png"
+  },
+  {
+    "revision": "54892660ff8f452dbd648ca21907a040",
+    "url": "/sbhairdesign-marketingsite/static/media/SalonPlaceholder.54892660.jpg"
+  },
+  {
+    "revision": "d001d51524b0df90a6b8739bb8e56416",
+    "url": "/sbhairdesign-marketingsite/static/media/Sarah.d001d515.jpg"
+  }
+]);
